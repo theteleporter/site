afb5ddc1b9c80e6cba15f9549d469af8154388b5
The Teleporter <m6253272@gmail.com>
2024-09-28 05:09:57 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         My Portfolio
       </h1>
       <p className="mb-4">
-        {`I’m a Next.js developer who is obsessed with the web and it's endless possibilities. I primarily write code for my personal projects and whatever ideas come to mind. I enjoy experimenting with new technologies and frameworks, continually learning and evolving my skills. Currently, I’m working on Xuneix, a security package designed to help developers and engineers secure their products, saving them time on setting up security across their projects.`}
+        {`I’m a Next.js developer who is obsessed with the web and it's endless possibilities. For the past few years, I have writen code mostly for my personal projects and whatever ideas came to mind. I enjoy experimenting with new technologies and frameworks, continually learning and evolving my skills. Currently, I’m working on Xuneix, a security package designed to help developers and engineers secure their products, saving them time on setting up security across their projects.`}
       </p>
       <div className="my-8">
         <BlogPosts />
