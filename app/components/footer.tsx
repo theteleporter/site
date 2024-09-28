8be4dfdd8d0566e05d1ab0e4fd4bb2ba2f748af5
The Teleporter <m6253272@gmail.com>
2024-09-28 03:33:16 +0300

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -46,15 +46,15 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://x.com/theteleporter_"
+            href="https://x.com/@theteleporter_"
           >
          {/** <ArrowIcon /> **/}
             <p className="ml-2 h-7">twitter</p>
           </a>
         </li>
       </ul>
-      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
+      <p className="mt-8 text-sm text-neutral-600 dark:text-neutral-300">
+        © {new Date().getFullYear()} The Teleporter
       </p>
     </footer>
   )
