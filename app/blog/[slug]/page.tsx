524c1986a473a0a11b47962f75234b43db3d1ce2
The Teleporter <m6253272@gmail.com>
2024-09-28 04:37:39 +0300

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -86,7 +86,7 @@ export default function Blog({ params }) {
       <Link href='/blog' className='rounded-full px-2 py-1 bg-zinc-900 border-stone-700 border text-sm'>
        All posts
       </Link>
-      <h1 className="title font-semibold text-2xl tracking-tighter mt-14">
+      <h1 className="title font-semibold text-2xl tracking-tighter mt-10">
         {post.metadata.title}
       </h1>
       <div className="flex justify-between items-center mt-2 mb-8 text-sm">
