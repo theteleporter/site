fdf9bc41b953cd16bebf6b8f7bcebfe033c1f184
The Teleporter <m6253272@gmail.com>
2024-09-28 03:47:02 +0300

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -17,19 +17,8 @@ function ArrowIcon() {
 
 export default function Footer() {
   return (
-    <footer className="mb-16 justify-end">
+    <footer className="mb-16">
       <ul className="font-sm text-sm mt-8 flex flex-row space-x-2 text-neutral-600 md:flex-row md:space-x-2 md:space-y-0 dark:text-neutral-300">
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="/rss"
-          >
-         {/** <ArrowIcon /> **/}
-            <p className="ml-2 h-7">rss</p>
-          </a>
-        </li>
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
@@ -53,9 +42,20 @@ export default function Footer() {
           </a>
         </li>
       </ul>
+      <div className="flex justify-between mx-auto">
       <p className="mt-8 text-xs text-neutral-600 dark:text-neutral-300">
         &copy; {new Date().getFullYear()} The Teleporter
       </p>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="/rss"
+          >
+         {/** <ArrowIcon /> **/}
+            <p className="ml-2 h-7">rss</p>
+          </a>
+        </div>
     </footer>
   )
 }
