cedfcf8dce1712e459c679cf4bfaf45d8a8b4c10
The Teleporter <m6253272@gmail.com>
2024-09-30 01:33:08 +0300

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,7 +2,7 @@ import { notFound } from 'next/navigation'
 import { CustomMDX } from 'app/components/mdx'
 import { formatDate, getBlogPosts } from 'app/blog/utils'
 import { baseUrl } from 'app/sitemap'
-import { Link } from 'next-view-transitions'
+import { Link, ViewTransitions } from 'next-view-transitions'
 
 export async function generateStaticParams() {
   let posts = getBlogPosts()
@@ -60,45 +60,57 @@ export default function Blog({ params }) {
   }
 
   return (
-    <section>
-      <script
-        type="application/ld+json"
-        suppressHydrationWarning
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            '@context': 'https://schema.org',
-            '@type': 'BlogPosting',
-            headline: post.metadata.title,
-            datePublished: post.metadata.publishedAt,
-            dateModified: post.metadata.publishedAt,
-            description: post.metadata.summary,
-            image: post.metadata.image
-              ? `${baseUrl}${post.metadata.image}`
-              : `/og?title=${encodeURIComponent(post.metadata.title)}`,
-            url: `${baseUrl}/blog/${post.slug}`,
-            author: {
-              '@type': 'Person',
-              name: 'The Teleporter',
-            },
-          }),
-        }}
-      />
-      <Link 
+    <ViewTransitions>
+      <section>
+        <script
+          type="application/ld+json"
+          suppressHydrationWarning
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'BlogPosting',
+              headline: post.metadata.title,
+              datePublished: post.metadata.publishedAt,
+              dateModified: post.metadata.publishedAt,
+              description: post.metadata.summary,
+              image: post.metadata.image
+                ? `${baseUrl}${post.metadata.image}`
+                : `/og?title=${encodeURIComponent(post.metadata.title)}`,
+              url: `${baseUrl}/blog/${post.slug}`,
+              author: {
+                '@type': 'Person',
+                name: 'The Teleporter',
+              },
+            }),
+          }}
+        />
+        <Link 
           href='/blog' 
-          className='rounded-full px-2 py-1 bg-zinc-900 hover:bg-zinc-800 border-stone-700 hover:border-none border text-sm transition-all duration-500 ease-out'>
-       All posts
-      </Link>
-      <h1 className="title font-semibold text-2xl tracking-tighter mt-8">
-        {post.metadata.title}
-      </h1>
-      <div className="flex justify-between items-center mt-2 mb-8 text-sm">
-        <p className="text-sm text-neutral-600 dark:text-neutral-400">
-          {formatDate(post.metadata.publishedAt)}
-        </p>
-      </div>
-      <article className="prose">
-        <CustomMDX source={post.content} />
-      </article>
-    </section>
+          className='rounded-full px-2 py-1 bg-zinc-900 hover:bg-zinc-800 border-stone-700 hover:border-none border text-sm transition-all duration-500 ease-out'
+        >
+          All posts
+        </Link>
+        <h1 
+          className="title font-semibold text-2xl tracking-tighter mt-8"
+          style={{ viewTransitionName: `title-${post.slug}` }}
+        >
+          {post.metadata.title}
+        </h1>
+        <div className="flex justify-between items-center mt-2 mb-8 text-sm">
+          <p 
+            className="text-sm text-neutral-600 dark:text-neutral-400"
+            style={{ viewTransitionName: `date-${post.slug}` }}
+          >
+            {formatDate(post.metadata.publishedAt)}
+          </p>
+        </div>
+        <article 
+          className="prose"
+          style={{ viewTransitionName: `content-${post.slug}` }}
+        >
+          <CustomMDX source={post.content} />
+        </article>
+      </section>
+    </ViewTransitions>
   )
 }
