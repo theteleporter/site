2dd64d517e4fae9f75c7168751fec8a178770b27
The Teleporter <m6253272@gmail.com>
2024-09-30 02:24:30 +0300

    Update nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -25,9 +25,6 @@ export function Navbar() {
                 <Link
                   key={path}
                   href={path}
-                  onClick={() => {
-        router.push(path)
-      }}
                   className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1 cursor-pointer nav-item"
                   style={{ viewTransitionName: `nav-${name}` }}
                 >
