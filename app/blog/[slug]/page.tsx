65435013e5721efd01c7f8af41bd066da3573bf8
The Teleporter <m6253272@gmail.com>
2024-09-30 02:53:55 +0300

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -87,7 +87,8 @@ export default function Blog({ params }) {
         />
         <div className="mb-8">
          <Link
- className='rounded-full px-2 py-1 bg-zinc-900 hover:bg-zinc-800 border-stone-700 hover:border-none border text-sm'
+prefetch={true}
+ className='rounded-full px-2 py-1 bg-zinc-900 hover:bg-zinc-800 border-stone-700 hover:border-none border text-sm transition-all duration-700 ease-out'
             style={{ viewTransitionName: 'all-posts-button' }}
  href='/blog'
 >
