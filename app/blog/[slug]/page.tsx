6fc958b9b128fe7171357e9ced5b57b928a42a3c
The Teleporter <m6253272@gmail.com>
2024-09-29 21:56:47 +0000

    next transitions -experimental

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,7 +2,7 @@ import { notFound } from 'next/navigation'
 import { CustomMDX } from 'app/components/mdx'
 import { formatDate, getBlogPosts } from 'app/blog/utils'
 import { baseUrl } from 'app/sitemap'
-import Link from 'next/link'
+import { Link } from 'next-view-transitions'
 
 export async function generateStaticParams() {
   let posts = getBlogPosts()
