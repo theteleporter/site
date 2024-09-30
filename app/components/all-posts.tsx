bf9494f1a10b3e2e0f843ce3d1c951eb69e0f8af
The Teleporter <m6253272@gmail.com>
2024-09-30 04:28:42 +0300

    Update all-posts.tsx

--- app/components/all-posts.tsx
+++ app/components/all-posts.tsx
@@ -1,7 +1,8 @@
-import { Link } from 'next-view-transitions'
+import { Link, ViewTransitions } from 'next-view-transitions'
 
 export function AllPosts(){
 return (
+<ViewTransitions>
  <Link
  className='rounded-full px-2 py-1 bg-zinc-900 hover:bg-zinc-800 border-stone-700 hover:border-none border text-sm transition-all duration-700 ease-out'
             style={{ viewTransitionName: 'all-posts-button' }}
@@ -9,5 +10,6 @@ return (
 >
         All posts
       </Link>
+</ViewTransitions>
 )
 }
