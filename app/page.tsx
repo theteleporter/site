c97972b51175b6fd3ca8ab39ab08c2126aab90b7
The Teleporter <m6253272@gmail.com>
2024-10-01 03:53:43 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,31 @@
 import { BlogPosts } from 'app/components/posts'
+import { baseUrl } from 'app/sitemap'
+
+export const metadata = {
+  title: 'The Teleporter',
+  description: 'I make crappy websites.',
+  openGraph: {
+    title: 'The Teleporter',
+    description: 'I make crappy websites.',
+    url: baseUrl,
+    siteName: 'The Teleporter',
+    images: [
+      {
+        url: `${baseUrl}/og?title=${encodeURIComponent('The Teleporter')}&date=${encodeURIComponent(new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }))}`,
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en-US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'The Teleporter',
+    description: 'I make crappy websites.',
+    images: [`${baseUrl}/og?title=${encodeURIComponent('The Teleporter')}&date=${encodeURIComponent(new Date().toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }))}`],
+  },
+}
 
 export default function Page() {
   return (
@@ -7,7 +34,7 @@ export default function Page() {
         About
       </h1>
       <p className="mb-4">
-        {`I’m a Next.js developer who is obsessed with the web and it's endless possibilities. For the past few years, I have writen code mostly for my personal projects and whatever ideas came to mind. I enjoy experimenting with new technologies and frameworks, continually learning and evolving my skills. Currently, I’m working on Xuneix, a security package designed to help developers and engineers secure their products, saving them time on setting up security across their projects.`}
+        {`I'm a Next.js developer who is obsessed with the web and its endless possibilities. For the past few years, I have written code mostly for my personal projects and whatever ideas came to mind. I enjoy experimenting with new technologies and frameworks, continually learning and evolving my skills. Currently, I'm working on Xuneix, a security package designed to help developers and engineers secure their products, saving them time on setting up security across their projects.`}
       </p>
       <div className="my-8">
         <BlogPosts />
