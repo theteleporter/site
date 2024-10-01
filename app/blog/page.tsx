af49601a9987c1d0ccb237af6143e2b8aad13b28
The Teleporter <m6253272@gmail.com>
2024-10-01 03:46:39 +0300

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,4 @@
-
-
 import { BlogPosts } from 'app/components/posts'
-import { ViewTransitions } from 'next-view-transitions'
 
 export const metadata = {
   title: 'Blog',
@@ -10,16 +7,9 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <ViewTransitions>
-      <section>
-        <h1 
-          className="font-semibold text-2xl mb-8 tracking-tighter"
-          style={{ viewTransitionName: 'all-posts-button' }}
-        >
-          All posts
-        </h1>
-        <BlogPosts />
-      </section>
-    </ViewTransitions>
+    <section>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Blog</h1>
+      <BlogPosts />
+    </section>
   )
 }
