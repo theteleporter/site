f6dd3b32f7b4becd6f0d8cd901fd3d5b6a8c3d0f
The Teleporter <m6253272@gmail.com>
2024-10-01 18:25:21 +0300

    Update posts.tsx

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -23,7 +23,7 @@ export function BlogPosts() {
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
               <p 
-                className="text-neutral-600 dark:text-neutral-400 w-[140px] tabular-nums"
+                className="text-neutral-600 dark:text-neutral-400 w-[150px] tabular-nums"
                 style={{ viewTransitionName: `date-${post.slug}` }}
               >
                 {formatDate(post.metadata.publishedAt, false)}
