cfc1f4453eb9068dcc40e0cae5c299d157fbb5f3
The Teleporter <m6253272@gmail.com>
2024-10-01 04:43:02 +0300

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -95,9 +95,9 @@ export default function Blog({ params }) {
             }),
           }}
         />
-        <div className="mb-8">
+      {/**  <div className="mb-8">
          <AllPosts />
-        </div>
+        </div> **/}
         <h1 
           className="title font-semibold text-2xl tracking-tighter mt-8"
           style={{ viewTransitionName: `title-${post.slug}` }}
