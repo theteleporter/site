625e3307ca4e4dfcc053497b3fadccd9e2ece85a
The Teleporter <m6253272@gmail.com>
2024-10-13 13:53:13 +0300

    Update components

---
+++ app/components/icons.tsx
@@ -0,0 +1,316 @@
+export function LogoInvert(props: BrandProps) {
+  return (
+    <>
+      <svg
+        id="uuid-255b3629-a4d8-41b9-bed7-7f9cf05f111e"
+        data-name="Layer 1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 1916.88 222.48"
+        className={props.className}
+      >
+        <defs>
+          <filter
+            id="uuid-99a6a4ab-eb8d-4e5c-b827-f94ebc5fe0c5"
+            data-name="drop-shadow-1"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dx="8" dy="8" />
+            <feGaussianBlur
+              result="uuid-9d590a94-3828-4615-bb82-275d98615b67"
+              stdDeviation="10"
+            />
+            <feFlood flood-color="#000" flood-opacity=".75" />
+            <feComposite
+              in2="uuid-9d590a94-3828-4615-bb82-275d98615b67"
+              operator="in"
+            />
+            <feComposite in="SourceGraphic" />
+          </filter>
+          <filter
+            id="uuid-a17d7ec8-6b0a-4578-920b-550f0897bb7d"
+            data-name="drop-shadow-2"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dx="8" dy="8" />
+            <feGaussianBlur
+              result="uuid-68fa0671-7d43-4c45-b734-3b998090ff43"
+              stdDeviation="10"
+            />
+            <feFlood flood-color="#000" flood-opacity=".75" />
+            <feComposite
+              in2="uuid-68fa0671-7d43-4c45-b734-3b998090ff43"
+              operator="in"
+            />
+            <feComposite in="SourceGraphic" />
+          </filter>
+          <filter
+            id="uuid-a74730f7-91f0-4db0-8ad0-5ac6eba3014f"
+            data-name="drop-shadow-3"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dx="8" dy="8" />
+            <feGaussianBlur
+              result="uuid-741214f7-8e29-4e7e-aa72-47608d23c9af"
+              stdDeviation="10"
+            />
+            <feFlood flood-color="#000" flood-opacity=".75" />
+            <feComposite
+              in2="uuid-741214f7-8e29-4e7e-aa72-47608d23c9af"
+              operator="in"
+            />
+            <feComposite in="SourceGraphic" />
+          </filter>
+          <filter
+            id="uuid-b725616f-6d83-49a7-b637-28f3db328b28"
+            data-name="drop-shadow-4"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dx="8" dy="8" />
+            <feGaussianBlur
+              result="uuid-b225c688-c195-4251-8d1a-0a36241d5bee"
+              stdDeviation="10"
+            />
+            <feFlood flood-color="#000" flood-opacity=".75" />
+            <feComposite
+              in2="uuid-b225c688-c195-4251-8d1a-0a36241d5bee"
+              operator="in"
+            />
+            <feComposite in="SourceGraphic" />
+          </filter>
+          <filter
+            id="uuid-1eaa28bc-83df-4c81-a657-a97befde426f"
+            data-name="drop-shadow-5"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dx="8" dy="8" />
+            <feGaussianBlur
+              result="uuid-05a2d6cf-b001-4281-a9ae-cb243e4e5b4e"
+              stdDeviation="10"
+            />
+            <feFlood flood-color="#000" flood-opacity=".75" />
+            <feComposite
+              in2="uuid-05a2d6cf-b001-4281-a9ae-cb243e4e5b4e"
+              operator="in"
+            />
+            <feComposite in="SourceGraphic" />
+          </filter>
+          <filter
+            id="uuid-36ad3f5b-5444-4164-aaef-5cd9cfd190ad"
+            data-name="drop-shadow-6"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dx="8" dy="8" />
+            <feGaussianBlur
+              result="uuid-c6c82a0a-2734-4506-b8c2-11d99cefedf5"
+              stdDeviation="10"
+            />
+            <feFlood flood-color="#000" flood-opacity=".75" />
+            <feComposite
+              in2="uuid-c6c82a0a-2734-4506-b8c2-11d99cefedf5"
+              operator="in"
+            />
+            <feComposite in="SourceGraphic" />
+          </filter>
+          <filter
+            id="uuid-7c5669e4-5855-4780-b374-f9797f7e86ca"
+            data-name="drop-shadow-7"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dx="8" dy="8" />
+            <feGaussianBlur
+              result="uuid-52ee0bf6-de55-4ec2-9201-971ecc68e933"
+              stdDeviation="10"
+            />
+            <feFlood flood-color="#000" flood-opacity=".75" />
+            <feComposite
+              in2="uuid-52ee0bf6-de55-4ec2-9201-971ecc68e933"
+              operator="in"
+            />
+            <feComposite in="SourceGraphic" />
+          </filter>
+          <filter
+            id="uuid-e7538209-23d7-4041-8ee4-4be7503874b9"
+            data-name="drop-shadow-8"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dx="8" dy="8" />
+            <feGaussianBlur
+              result="uuid-612087e2-6dbc-416a-be74-7e2b1a8af39b"
+              stdDeviation="10"
+            />
+            <feFlood flood-color="#000" flood-opacity=".75" />
+            <feComposite
+              in2="uuid-612087e2-6dbc-416a-be74-7e2b1a8af39b"
+              operator="in"
+            />
+            <feComposite in="SourceGraphic" />
+          </filter>
+          <filter
+            id="uuid-d5ccd03f-264b-45be-97bb-d6ceec42f512"
+            data-name="drop-shadow-9"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dx="8" dy="8" />
+            <feGaussianBlur
+              result="uuid-7d463de1-be86-4f22-9f77-4afaa24b10c9"
+              stdDeviation="10"
+            />
+            <feFlood flood-color="#000" flood-opacity=".75" />
+            <feComposite
+              in2="uuid-7d463de1-be86-4f22-9f77-4afaa24b10c9"
+              operator="in"
+            />
+            <feComposite in="SourceGraphic" />
+          </filter>
+          <filter
+            id="uuid-b004ac17-31b0-4e54-9ea0-19fff5b632a2"
+            data-name="drop-shadow-10"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dx="8" dy="8" />
+            <feGaussianBlur
+              result="uuid-ed418c6b-135e-44ad-b4fc-d096f41fac39"
+              stdDeviation="10"
+            />
+            <feFlood flood-color="#000" flood-opacity=".75" />
+            <feComposite
+              in2="uuid-ed418c6b-135e-44ad-b4fc-d096f41fac39"
+              operator="in"
+            />
+            <feComposite in="SourceGraphic" />
+          </filter>
+          <filter
+            id="uuid-17bf2705-2b90-4c43-a4ee-d2196ba17787"
+            data-name="drop-shadow-11"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dx="8" dy="8" />
+            <feGaussianBlur
+              result="uuid-7c9bf7b8-45d9-4a0b-9de6-2067eb84fccf"
+              stdDeviation="10"
+            />
+            <feFlood flood-color="#000" flood-opacity=".75" />
+            <feComposite
+              in2="uuid-7c9bf7b8-45d9-4a0b-9de6-2067eb84fccf"
+              operator="in"
+            />
+            <feComposite in="SourceGraphic" />
+          </filter>
+          <filter
+            id="uuid-b063351d-b06c-478b-91cb-cec7bbde3624"
+            data-name="drop-shadow-12"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dx="8" dy="8" />
+            <feGaussianBlur
+              result="uuid-c9654528-08dc-4535-8aeb-026658a306ff"
+              stdDeviation="10"
+            />
+            <feFlood flood-color="#000" flood-opacity=".75" />
+            <feComposite
+              in2="uuid-c9654528-08dc-4535-8aeb-026658a306ff"
+              operator="in"
+            />
+            <feComposite in="SourceGraphic" />
+          </filter>
+          <filter
+            id="uuid-82541749-97c6-4759-b6a2-b7c4250958c8"
+            data-name="drop-shadow-13"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dx="8" dy="8" />
+            <feGaussianBlur
+              result="uuid-187faca1-f917-4777-923a-9e62e1e50e1a"
+              stdDeviation="10"
+            />
+            <feFlood flood-color="#000" flood-opacity=".75" />
+            <feComposite
+              in2="uuid-187faca1-f917-4777-923a-9e62e1e50e1a"
+              operator="in"
+            />
+            <feComposite in="SourceGraphic" />
+          </filter>
+        </defs>
+        <path
+          d="M22.12,25.54h121.88v33.22h-42.46v120.78h-37.18V58.76H22.12V25.54Z"
+          fill="#e6e6e6"
+          filter="url(#uuid-99a6a4ab-eb8d-4e5c-b827-f94ebc5fe0c5)"
+          stroke-width="0"
+        />
+        <path
+          d="M198.12,25.54v154h-37.18V25.54h37.18ZM171.28,81.64h116.6v33.44h-116.6v-33.44ZM301.52,25.54v154h-37.18V25.54h37.18Z"
+          fill="#e6e6e6"
+          filter="url(#uuid-a17d7ec8-6b0a-4578-920b-550f0897bb7d)"
+          stroke-width="0"
+        />
+        <path
+          d="M369.5,25.54v154h-35.64V25.54h35.64ZM358.5,25.54h77.66v30.58h-77.66v-30.58ZM358.5,83.62h73.26v29.92h-73.26v-29.92ZM358.5,148.96h77.66v30.58h-77.66v-30.58Z"
+          fill="#e6e6e6"
+          filter="url(#uuid-a74730f7-91f0-4db0-8ad0-5ac6eba3014f)"
+          stroke-width="0"
+        />
+        <path
+          d="M517.11,25.54h121.88v33.22h-42.46v120.78h-37.18V58.76h-42.24V25.54Z"
+          fill="#e6e6e6"
+          filter="url(#uuid-b725616f-6d83-49a7-b637-28f3db328b28)"
+          stroke-width="0"
+        />
+        <path
+          d="M691.57,25.54v154h-35.64V25.54h35.64ZM680.57,25.54h77.66v30.58h-77.66v-30.58ZM680.57,83.62h73.26v29.92h-73.26v-29.92ZM680.57,148.96h77.66v30.58h-77.66v-30.58Z"
+          fill="#e6e6e6"
+          filter="url(#uuid-1eaa28bc-83df-4c81-a657-a97befde426f)"
+          stroke-width="0"
+        />
+        <path
+          d="M825.77,25.54v122.1h61.82v31.9h-99V25.54h37.18Z"
+          fill="#e6e6e6"
+          filter="url(#uuid-36ad3f5b-5444-4164-aaef-5cd9cfd190ad)"
+          stroke-width="0"
+        />
+        <path
+          d="M940.39,25.54v154h-35.64V25.54h35.64ZM929.39,25.54h77.66v30.58h-77.66v-30.58ZM866.4,95.54h190v29h-190v-29ZM929.39,148.96h77.66v30.58h-77.66v-30.58Z"
+          fill="#e6e6e6"
+          filter="url(#uuid-7c5669e4-5855-4780-b374-f9797f7e86ca)"
+          stroke-width="0"
+        />
+        <path
+          d="M1074.59,25.54v154h-37.18V25.54h37.18ZM1060.73,25.54h30.14c13.05,0,24.01,2.02,32.89,6.05,8.87,4.04,15.58,9.79,20.13,17.27,4.54,7.48,6.82,16.5,6.82,27.06s-2.28,19.18-6.82,26.73c-4.55,7.56-11.26,13.35-20.13,17.38-8.88,4.04-19.84,6.05-32.89,6.05h-30.14v-30.58h30.14c7.48,0,13.34-1.68,17.6-5.06,4.25-3.37,6.38-8.21,6.38-14.52s-2.13-11.36-6.38-14.74c-4.26-3.37-10.12-5.06-17.6-5.06h-30.14v-30.58Z"
+          fill="#e6e6e6"
+          filter="url(#uuid-e7538209-23d7-4041-8ee4-4be7503874b9)"
+          stroke-width="0"
+        />
+        <path
+          d="M1171.94,69.98c4.18-9.82,10.01-18.33,17.49-25.52,7.48-7.18,16.28-12.72,26.4-16.61,10.12-3.88,21.04-5.83,32.78-5.83s22.84,1.95,32.89,5.83c10.04,3.89,18.81,9.43,26.29,16.61,7.48,7.19,13.27,15.7,17.38,25.52,4.1,9.83,6.16,20.68,6.16,32.56s-2.02,22.81-6.05,32.78c-4.04,9.98-9.76,18.63-17.16,25.96-7.41,7.34-16.17,13.02-26.29,17.05s-21.2,6.05-33.22,6.05-23.32-2.02-33.44-6.05c-10.12-4.03-18.89-9.71-26.29-17.05-7.41-7.33-13.13-15.98-17.16-25.96-4.04-9.97-6.05-20.9-6.05-32.78s2.09-22.73,6.27-32.56ZM1272.94,74.92c1.69,3,4.08,5.36,7.16,7.06,3.08,1.71,6.7,2.56,10.85,2.56s7.66-.85,10.74-2.56c3.08-1.71,5.47-4.06,7.16-7.06,1.69-3,2.54-6.46,2.54-10.38s-.83-7.38-2.49-10.38c-1.66-3-4.01-5.36-7.06-7.06-3.05-1.71-6.68-2.56-10.9-2.56s-7.77.85-10.85,2.56c-3.08,1.71-5.47,4.06-7.16,7.06-1.7,3-2.54,6.46-2.54,10.38s.85,7.38,2.54,10.38Z"
+          fill="#e6e6e6"
+          filter="url(#uuid-d5ccd03f-264b-45be-97bb-d6ceec42f512)"
+          stroke-width="0"
+        />
+        <path
+          d="M1378.41,25.54h32.78c13.2,0,24.2,2.13,33,6.38,8.8,4.26,15.47,10.16,20.02,17.71,4.54,7.56,6.82,16.32,6.82,26.29s-2.28,18.52-6.82,26.07c-4.55,7.56-11.22,13.46-20.02,17.71-8.8,4.26-19.8,6.38-33,6.38h-32.78v-28.82h30.14c4.98,0,9.27-.77,12.87-2.31,3.59-1.54,6.34-3.81,8.25-6.82,1.9-3,2.86-6.63,2.86-10.89s-.96-7.88-2.86-10.89c-1.91-3-4.66-5.31-8.25-6.93-3.6-1.61-7.89-2.42-12.87-2.42h-30.14v-31.46ZM1432.09,109.8l48.18,69.74h-43.56l-43.12-69.74h38.5Z"
+          fill="#e6e6e6"
+          filter="url(#uuid-b004ac17-31b0-4e54-9ea0-19fff5b632a2)"
+          stroke-width="0"
+        />
+        <path
+          d="M1482.02,25.54h121.88v33.22h-42.46v120.78h-37.18V58.76h-42.24V25.54Z"
+          fill="#e6e6e6"
+          filter="url(#uuid-17bf2705-2b90-4c43-a4ee-d2196ba17787)"
+          stroke-width="0"
+        />
+        <path
+          d="M1656.48,25.54v154h-35.64V25.54h35.64ZM1645.48,25.54h77.66v30.58h-77.66v-30.58ZM1645.48,83.62h73.26v29.92h-73.26v-29.92ZM1645.48,148.96h77.66v30.58h-77.66v-30.58Z"
+          fill="#e6e6e6"
+          filter="url(#uuid-b063351d-b06c-478b-91cb-cec7bbde3624)"
+          stroke-width="0"
+        />
+        <path
+          d="M1776.82,25.54h32.78c13.2,0,24.2,2.13,33,6.38,8.8,4.26,15.47,10.16,20.02,17.71,4.54,7.56,6.82,16.32,6.82,26.29s-2.28,18.52-6.82,26.07c-4.55,7.56-11.22,13.46-20.02,17.71-8.8,4.26-19.8,6.38-33,6.38h-32.78v-28.82h30.14c4.98,0,9.27-.77,12.87-2.31,3.59-1.54,6.34-3.81,8.25-6.82,1.9-3,2.86-6.63,2.86-10.89s-.96-7.88-2.86-10.89c-1.91-3-4.66-5.31-8.25-6.93-3.6-1.61-7.89-2.42-12.87-2.42h-30.14v-31.46ZM1830.5,109.8l48.18,69.74h-43.56l-43.12-69.74h38.5Z"
+          fill="#e6e6e6"
+          filter="url(#uuid-82541749-97c6-4759-b6a2-b7c4250958c8)"
+          stroke-width="0"
+        />
+      </svg>
+    </>
+  );
+}
