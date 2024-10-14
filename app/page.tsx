397c608f62487b4d9173bd13d97f158821fb7057
The Teleporter <m6253272@gmail.com>
2024-10-14 06:51:14 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -31,8 +31,8 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-       <LogoInvert className={"w-52 h-auto navbar-logo"} />
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter"> About
+       {/**<LogoInvert className={"w-52 h-auto navbar-logo"} />**/}
       </h1>
       <p className="mb-4">
         {`I'm a frontend developer who is obsessed with the web and its endless possibilities. For the past few years, I have written code mostly for my personal projects and whatever ideas came to mind. I enjoy experimenting with new technologies and frameworks, continually learning and evolving my skills. Currently, I'm working on Xuneix, a security package designed to help developers and engineers secure their products, saving them time on setting up security across their projects.`}
