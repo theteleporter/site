4f61c2452c6b4f44f1016d3cecde0cba7b1acba2
The Teleporter <m6253272@gmail.com>
2024-10-29 19:42:42 +0300

    Update

---
+++ next.config.ts
@@ -0,0 +1,26 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/affiliate-programs',
+        destination: '/blog',
+        permanent: true,
+      },
+      {
+        source: '/github',
+        destination: 'https://github.com/theteleporter',
+        permanent: true,
+      },
+      {
+        source: '/x',
+        destination: 'https://x.com/@theteleporter_',
+        permanent: true,
+      },
+    ]
+  },
+}
+}
+ 
+module.exports = nextConfig
