7fe6e6a8c048e558df78e6ab03481e31ae2676fb
The Teleporter <m6253272@gmail.com>
2024-11-06 01:57:23 +0300

    Update morph-tooltip.tsx

--- app/components/morph-tooltip.tsx
+++ app/components/morph-tooltip.tsx
@@ -62,7 +62,7 @@ const MorphTooltip = ({ children, content, visible }: MorphTooltipProps) => {
         <Tooltip.Portal>
           <Tooltip.Content sideOffset={5} className="tooltip-content">
             <div
-              className="text-black dark:bg-[#232323] dark:text-white bg-white border-[#212121] shadow-lg rounded-lg"
+              className="text-black dark:bg-[#232323] dark:text-white bg-white border-[#262626] shadow-lg rounded-lg"
               style={{
                 padding: '8px 12px',
                 fontSize: '13px',
