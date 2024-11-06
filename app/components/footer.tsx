c61f872d5249b75f442195f18492b3af568ded01
The Teleporter <m6253272@gmail.com>
2024-11-06 03:48:50 +0300

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -49,7 +49,7 @@ export default function Footer() {
         <p style={{ viewTransitionName: 'footer-copyright' }}>
           &copy; {new Date().getFullYear()} The Teleporter
         </p>
-        {/**<ThemeToggler />**/}
+        <ThemeToggler />
         <Link
           className="flex items-center transition-all duration-500 ease-out hover:text-neutral-800 dark:hover:text-neutral-100 cursor-pointer footer-link"
           href="/rss"
