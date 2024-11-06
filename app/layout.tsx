062a8b90eafe712680e247ec74e1c9c7cffd5da8
The Teleporter <m6253272@gmail.com>
2024-11-06 05:17:55 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,6 +62,8 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="system"
           enableSystem
+          enableColorScheme
+          themes={['light', 'dark', 'system']}
         >
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
