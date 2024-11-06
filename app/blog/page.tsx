651091d7874a851af9586774d8889487ec91fd8c
The Teleporter <m6253272@gmail.com>
2024-11-06 15:57:33 +0300

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -30,7 +30,7 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tight">Blog</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Blog</h1>
       <BlogPosts />
     </section>
   )
