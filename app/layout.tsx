cc7227aa5259b47c95a4a991bbeea244189e00f6
The Teleporter <m6253272@gmail.com>
2024-11-06 10:58:33 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,6 @@ export default function RootLayout({
         )}
         suppressHydrationWarning
       >
-        <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
@@ -63,6 +62,7 @@ export default function RootLayout({
             enableColorScheme
             themes={['light', 'dark', 'system']}
           >
+        <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
             <div className="text-black bg-white dark:text-white dark:bg-[#161614]">
               <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
                 <Navbar />
@@ -72,8 +72,8 @@ export default function RootLayout({
                 <SpeedInsights />
               </main>
             </div>
-          </ThemeProvider>
         </body>
+      </ThemeProvider>
       </html>
     </ViewTransitions>
   )
