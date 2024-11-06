aa5b114f97e0116d5972a86ae093a79b7f89b7b8
The Teleporter <m6253272@gmail.com>
2024-11-06 11:24:10 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
         className={cx(
           GeistSans.variable,
           GeistMono.variable
-        , `text-black bg-white`) }
+           ) }
         suppressHydrationWarning
       >
         <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
@@ -63,7 +63,7 @@ export default function RootLayout({
             enableColorScheme
             themes={['light', 'dark', 'system']}
           >
-            <div className="">
+            <div className="text-black bg-white dark:text-white dark:bg-[#161614]">
               <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
                 <Navbar />
                 {children}
