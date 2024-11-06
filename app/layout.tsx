864a4e17aab9258ea4e6d1e6d73ee9907a7fd56b
The Teleporter <m6253272@gmail.com>
2024-11-06 10:52:36 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,34 +46,35 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-   <ViewTransitions>
-    <html
-      lang="en"
-      className={cx(
-        'text-black bg-white dark:text-white dark:bg-[#161614]',
-        GeistSans.variable,
-        GeistMono.variable
-      )}
-    suppressHydrationWarning
-    >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto text-black bg-white dark:text-white dark:bg-[#161614]">
-      <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          enableColorScheme
-          themes={['light', 'dark', 'system']}
-        >
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
-          {children}
-          <Footer />
-          <Analytics />
-          <SpeedInsights />
-        </main>
-       </ThemeProvider>
-      </body>
-    </html>
-</ViewTransitions>
+    <ViewTransitions>
+      <html
+        lang="en"
+        className={cx(
+          GeistSans.variable,
+          GeistMono.variable
+        )}
+        suppressHydrationWarning
+      >
+        <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            enableColorScheme
+            themes={['light', 'dark', 'system']}
+          >
+            <div className="text-black bg-white dark:text-white dark:bg-[#161614]">
+              <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+                <Navbar />
+                {children}
+                <Footer />
+                <Analytics />
+                <SpeedInsights />
+              </main>
+            </div>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ViewTransitions>
   )
 }
